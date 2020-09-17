675b745162be3c12a31ba2f3c9fef6d7f12ff657
lemon-piepie <purplechilde@163.com>
2020-09-17 16:54:57 +0800

    finish practice1

--- src/main/java/com/thoughtworks/capability/gtb/Practice1.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice1.java
@@ -1,6 +1,9 @@
 package com.thoughtworks.capability.gtb;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
 
 /**
  * 计算任意日期与下一个劳动节相差多少天
@@ -11,6 +14,13 @@ import java.time.LocalDate;
 public class Practice1 {
 
   public static long getDaysBetweenNextLaborDay(LocalDate date) {
-    return 0;
+    LocalDate nextMayFirstDay;
+    if ( date.isBefore(LocalDate.of(date.getYear(),5,1))){
+      nextMayFirstDay = LocalDate.of(date.getYear(),5,1);
+    }else{
+      nextMayFirstDay = LocalDate.of(date.getYear()+1,5,1);
+    }
+    long daysBetweenNextLaborDay = date.until(nextMayFirstDay, ChronoUnit.DAYS);
+    return daysBetweenNextLaborDay;
   }
 }
